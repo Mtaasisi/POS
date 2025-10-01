d055c561d13588f2f6b3081aa6fe361e0cb18489
Mtaasisi <142012743+Mtaasisi@users.noreply.github.com>
2025-10-01 10:26:08 +0300

    Remove large files and update gitignore

--- crm_system/launch_dashboard.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "/Users/mtaasisi/Desktop/LATS CHANCE copy/crm_system"
-streamlit run dashboards/crm_dashboard.py --server.port 8501
--- crm_system/test_automation.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "/Users/mtaasisi/Desktop/LATS CHANCE copy/crm_system"
-python3 automation/marketing_automation.py
